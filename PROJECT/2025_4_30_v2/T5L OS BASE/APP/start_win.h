47a47e2c671f6b106f11ca74f274e16e200fccf6
高足 <gaozoo@126.com>
2025-04-30 17:34:32 +0800

    4.30.v2

---
+++ PROJECT/2025_4_30_v2/T5L OS BASE/APP/start_win.h
@@ -0,0 +1,18 @@
+#ifndef __START_WIN_H__
+#define __START_WIN_H__
+#include "sys.h"
+
+
+#define tubiao_chang 580	//图表的长度
+#define tubiao_kuan 120	//图表的宽度
+#define tubiao_zuoshang_x 10	//图表的左上x坐标
+#define tubiao_zuoshang_y 346	//图表的左上y坐标
+#define tubiao_0zuobiao_x 10 //0点坐标x
+#define tubiao_0zuobiao_y 426 //0点坐标y
+//高度6段 2.5 -1
+
+#define CHART_1_ADDR		0xb000
+#define CHART_2_ADDR		0x5500
+
+
+#endif
